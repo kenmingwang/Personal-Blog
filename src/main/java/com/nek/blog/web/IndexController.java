a59c9f39cbdfbdc2088a64607323be9300af8f5f
Nek <fengfeng8867dd@hotmail.com>
2018-10-13 11:09:28 -0600

    First commit on Personal blog

---
+++ src/main/java/com/nek/blog/web/IndexController.java
@@ -0,0 +1,24 @@
+package com.nek.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return  "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return  "blog";
+    }
+
+    @GetMapping("/Category")
+    public String types(){
+        return  "types";
+    }
+}
